fdd787bbe94f1c1f936e21a07b5f5b286cda117f
Joseph Williams <josephwilliamsjs@gmail.com>
2021-05-03 14:49:45 -0400

    strings 2 and analyzer

---
+++ analyzer.rb
@@ -0,0 +1,8 @@
+puts "Enter your first name"
+first_name = gets.chomp
+puts "Enter your last name"
+last_name = gets.chomp
+puts "Your full name is #{first_name} #{last_name}"
+puts "Your full name reveresed is #{first_name.reverse} #{last_name.reverse}"
+full_name = "#{first_name}#{last_name}"
+puts "Your name has #{full_name.length} characters in it"
---
+++ strings_2.rb
@@ -0,0 +1,9 @@
+# getting input from user
+puts "what is your first name?"
+first_name = gets.chomp
+puts "Thank you, you said your first name is #{first_name}"
+
+
+# puts "Enter a number to multiply by 2"
+# input = gets.chomp
+# puts input.to_i * 2  
