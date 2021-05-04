8a0887eff3d17995e1ae0fe39c1b02827b0744fe
Joseph Williams <josephwilliamsjs@gmail.com>
2021-05-04 11:46:20 -0400

    branching_simple_calculator

---
+++ branching_if_else.rb
@@ -0,0 +1,28 @@
+condition = true
+another_condition = false
+
+if !condition && another_condition
+  puts "Hello"
+else
+  puts "Bye" 
+end
+puts "la la la"
+
+if condition || another_condition
+  puts "Hello"
+else
+  puts "Bye" 
+end
+puts "la la la"
+
+
+name = "Brian"
+
+if name == "Joe"
+  puts "Welcome to the program, Joe"
+elsif name == "Jack"
+  puts "Welcome to the program, Jack"
+else
+  puts "Welcome to the program, User"
+end
+ 
---
+++ simple_calculator.rb
@@ -0,0 +1,51 @@
+def multiply(first_num, second_num)
+  first_num.to_f * second_num.to_f
+end
+
+def divide(first_num, second_num)
+  first_num.to_f / second_num.to_f
+end
+
+def subtract(first_num, second_num)
+  second_num.to_f - first_num.to_f
+end
+
+def add(first_num, second_num)
+  first_num.to_f + second_num.to_f
+end
+
+def mod(first_num, second_num)
+  first_num.to_f % second_num.to_f
+end
+
+
+puts "Simple calculator"
+25.times { print "-" }
+puts
+puts "Enter the first number"
+num_1 = gets.chomp
+puts "Enter the second number"
+num_2 = gets.chomp
+puts "What do you want to do?"
+puts "Enter 1 for multiply, 2 for addition, 3 for subtraction, 4 for division, 5 for mod"
+user_entry = gets.chomp
+
+if user_entry == "1"
+  puts "Yoy have chosen to multiply"
+  puts "The first number multiplied by the second number is #{multiply(num_1, num_2)}"
+elsif user_entry == "2"
+  puts "You have chosen to add"
+  puts "The first number added by the second number is #{add(num_1, num_2)}"
+elsif user_entry == "3"
+  puts "You have chosen to subtract"
+  puts "The first number subtracted by the second number is #{subtract(num_1, num_2)}"
+elsif user_entry == "4"
+  puts "You have chosen to divide"
+  puts "The first number divided by the second number is #{divide(num_1, num_2)}"
+elsif user_entry == "5"
+  puts "You have chosen to mod"
+  puts "The first number mod the second number is #{mod(num_1, num_2)}"
+else
+  puts "Invalid entry"
+end
+
